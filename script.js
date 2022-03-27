4c9215984c03bf5adcc31c17e27ec4b61b4d412e
alexmoDev <101904524+alexmoDev@users.noreply.github.com>
2022-03-26 22:36:55 -0300

    arquivos certificad

---
+++ script.js
@@ -0,0 +1,5 @@
+// Terminar todo o design que está no figma (disponível abaixo)
+// Adicionar suas redes sociais de forma que quem clique consiga abrí-las
+function tema() {
+  document.body.classList.toggle("dark");
+}
